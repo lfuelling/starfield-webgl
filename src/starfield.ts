c837a05ed55f6a599a29dd985049f84ac8111bee
Lukas Fülling <lukas@k40s.net>
2022-04-16 23:16:01 +0200

    cleanup animlopp

--- src/starfield.ts
+++ src/starfield.ts
@@ -26,27 +26,29 @@ export const runStarfield = (options: StarfieldOptions) => {
     let time = Date.now();
     let stars: Star[] = generateStars(settings, canvas);
 
+    const draw = (star: number[]) => {
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(star), gl.DYNAMIC_DRAW)
+
+        gl.enableVertexAttribArray(positionAttribute)
+        gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 6 * 4, 0)
+
+        gl.enableVertexAttribArray(colorAttribute)
+        gl.vertexAttribPointer(colorAttribute, 4, gl.FLOAT, false, 6 * 4, 2 * 4)
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW)
+
+        gl.drawElements(gl.TRIANGLES, indexArray.length, gl.UNSIGNED_SHORT, 0);
+    };
+
     // define animation loop
     const animLoop = () => {
         requestAnimationFrame(() => {
             stars.forEach(s => s.move(time));
             time = Date.now();
             clearCanvas(gl);
-            stars.map(s => s.getVertex()).forEach((star: number[]) => {
-                gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(star), gl.DYNAMIC_DRAW)
-
-                gl.enableVertexAttribArray(positionAttribute)
-                gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 6 * 4, 0)
-
-                gl.enableVertexAttribArray(colorAttribute)
-                gl.vertexAttribPointer(colorAttribute, 4, gl.FLOAT, false, 6 * 4, 2 * 4)
-
-                gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-                gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexArray), gl.STATIC_DRAW)
-
-                gl.drawElements(gl.TRIANGLES, indexArray.length, gl.UNSIGNED_SHORT, 0);
-            });
+            stars.map(s => s.getVertex()).forEach(draw);
 
             animLoop();
         });
